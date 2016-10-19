e932cc254a35f6bdf88019493195a9e45d1fde67
Ondrej Ponteš <opontes@redhat.com>
2016-10-19 11:17:46 +0200

    Created simple structure

---
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/ExapleDaoImpl.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.dao;
+
+/**
+ * @author opontes
+ */
+public class ExapleDaoImpl implements ExampleDao {
+}
---
+++ persistence/src/main/java/cz/muni/fi/pa165/enums/ExampleEnum.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.enums;
+
+/**
+ * @author opontes
+ */
+public enum ExampleEnum {
+}
