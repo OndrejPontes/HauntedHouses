c31d7d4fbb7a40fa3810b60c8768c426f258f62a
MonikaMociarikova <374485@mail.muni.cz>
2016-10-24 11:20:46 +0200

    Haunting entity and dao added, AbilityTestDao added

---
+++ persistence/src/main/java/cz/muni/fi/pa165/dao/HauntingDaoImpl.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.dao;
+
+/**
+ * Created by MONNY on 24-Oct-16.
+ */
+public class HauntingDaoImpl {
+}
---
+++ persistence/src/test/java/cz/muni/fi/pa165/AbilityDaoTest.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165;
+
+/**
+ * Created by MONNY on 24-Oct-16.
+ */
+public class AbilityDaoTest {
+}
