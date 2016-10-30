ceab9eab8785d702357792f4776a7bcc0cd6b196
Jirka Kruml <jirka.kruml@gmail.com>
2016-10-30 23:13:28 +0100

    Make name and history of house unique

--- persistence/src/main/java/cz/muni/fi/pa165/entity/House.java
+++ persistence/src/main/java/cz/muni/fi/pa165/entity/House.java
@@ -15,17 +15,17 @@ public class House {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String address;
 
     @Column(nullable = false)
     @Temporal(TemporalType.DATE)
     private Date hauntingFrom;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String history;
 
     @OneToMany
