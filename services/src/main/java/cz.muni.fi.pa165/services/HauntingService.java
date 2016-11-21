afabb188259180900f884208cc6951dde51e74fd
MonikaMociarikova <374485@mail.muni.cz>
2016-11-21 10:27:43 +0100

    Haunting service, Mapping service added

---
+++ services/src/main/java/cz.muni.fi.pa165/services/HauntingService.java
@@ -0,0 +1,28 @@
+package cz.muni.fi.pa165.services;
+
+import cz.muni.fi.pa165.entity.Haunting;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author MonikaMociarikova
+ *
+ * An interface that defines a service access to the {@link Haunting} entity.
+ */
+
+@Service
+public interface HauntingService {
+
+    void create(Haunting haunting);
+
+    void remove(Haunting haunting);
+
+    Haunting update(Haunting haunting);
+
+    Haunting findById(Long id);
+
+    List<Haunting> findByDate (Date date);
+
+    List<Haunting> findAll();
+}
