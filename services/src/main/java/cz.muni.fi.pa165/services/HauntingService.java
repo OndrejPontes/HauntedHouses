9ba042781d53c6b388e8191304f8dd3752499185
MonikaMociarikova <374485@mail.muni.cz>
2016-11-22 22:31:04 +0100

    Find renamed to get

--- services/src/main/java/cz.muni.fi.pa165/services/HauntingService.java
+++ services/src/main/java/cz.muni.fi.pa165/services/HauntingService.java
@@ -3,7 +3,8 @@ package cz.muni.fi.pa165.services;
 import cz.muni.fi.pa165.entity.Ghost;
 import cz.muni.fi.pa165.entity.Haunting;
 import org.springframework.stereotype.Service;
-import java.util.Date;
+
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -25,11 +26,11 @@ public interface HauntingService {
 
     Haunting update(Haunting haunting);
 
-    Haunting findById(Long id);
+    Haunting getById(Long id);
 
-    List<Haunting> findByDate (Date date);
+    List<Haunting> getByDate(Calendar date);
 
-    List<Haunting> findAll();
+    List<Haunting> getAll();
 
 
 }
