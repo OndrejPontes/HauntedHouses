95ac0d9519394b65aed3407cde190916427a43af
Ondrej Ponteš <opontes@redhat.com>
2016-11-22 12:57:16 +0100

    Created tests for Haunting

---
+++ services/src/test/java/cz.muni.fi.pa165/services/HauntingServiceTest.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pa165.services;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * @author Ondrej Ponteš
+ */
+@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(classes = {HauntingService.class})
+public class HauntingServiceTest {
+
+}
