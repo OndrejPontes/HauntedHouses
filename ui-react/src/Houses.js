d9cda882a6410089d06d3430badaa476cab00adc
Ondrej Ponteš <opontes@redhat.com>
2016-12-15 16:57:09 +0100

    Add buttons into UI

--- ui-react/src/Houses.js
+++ ui-react/src/Houses.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
-import { Table } from 'react-bootstrap'
+import { Table, Button, Glyphicon } from 'react-bootstrap'
 import axios from 'axios'
 
 class Houses extends Component {
-
-  constructor() {
+  constructor(){
     super();
-    axios.get(`http://localhost:8080/houses`)
+    this.state = {
+      houses: [],
+      showModal: false
+    }
+  }
+
+  closeModal(){
+    this.setState({ showModal: false });
+  }
+
+  openModal(){
+    this.setState({ showModal: true });
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:8080/pa165/houses`)
       .then(res => {
-        console.log(res.data);
+        // console.log(res.data);
         // const houses = res.data;
         this.setState({ houses: res.data });
         // const posts = res.data.data.children.map(obj => obj.data);
@@ -18,21 +32,35 @@ class Houses extends Component {
 
   render() {
     let tableLines = [];
-    console.log(this.state);
-    this.state.houses.forEach(function (house) {
-      console.log(house);
+    this.state.houses.forEach(function (house, i) {
       tableLines.push(
-        <tr>
-          <th>house.id</th>
-          <th>house.name</th>
-          <th>house.address</th>
-          <th>house.history</th>
-          <th>house.hauntingFrom</th>
+        <tr key={i}>
+          <th>{ house.id }</th>
+          <th>{ house.name }</th>
+          <th>{ house.address }</th>
+          <th>{ house.history }</th>
+          <th>{ house.hauntingFrom }</th>
+          <th>
+            <Button><Glyphicon glyph="pencil"/></Button>
+            <Button><Glyphicon glyph="remove"/></Button>
+          </th>
         </tr>
       )
     });
     return (
       <div>
+        <Modal show={this.state.showModal} onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div>Modal content here </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+
         <Table responsive>
           <thead>
           <tr>
@@ -41,6 +69,7 @@ class Houses extends Component {
             <th>Address</th>
             <th>History</th>
             <th>Haunting from</th>
+            <th><Button>New <Glyphicon glyph="plus"/></Button></th>
           </tr>
           </thead>
           <tbody>
