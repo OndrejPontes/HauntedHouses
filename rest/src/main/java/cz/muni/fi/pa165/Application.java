6c1381980ea71b9509afff44754b31af903acd7e
Vojtisek <vojta.david@gmail.com>
2016-12-15 10:50:04 +0100

    nyni funguje mvn tomcat7:run a http://localhost:8080/pa165

--- rest/src/main/java/cz/muni/fi/pa165/Application.java
+++ rest/src/main/java/cz/muni/fi/pa165/Application.java
@@ -2,6 +2,8 @@ package cz.muni.fi.pa165;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -10,10 +12,14 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(
         basePackages = {"cz.muni.fi.pa165.controller", "cz.muni.fi.pa165.facade", "cz.muni.fi.pa165.services", "cz.muni.fi.pa165.dao"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
