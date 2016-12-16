855a79a77ced191a50bbd5acc868d9ad37e92e1d
Ondrej Ponteš <opontes@redhat.com>
2016-12-16 03:29:10 +0100

    House UI is available

--- rest/src/main/java/cz/muni/fi/pa165/controller/HouseController.java
+++ rest/src/main/java/cz/muni/fi/pa165/controller/HouseController.java
@@ -1,16 +1,14 @@
 package cz.muni.fi.pa165.controller;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import cz.muni.fi.pa165.ApiUris;
+import cz.muni.fi.pa165.dto.HouseCreateDTO;
+import cz.muni.fi.pa165.dto.HouseDTO;
+import cz.muni.fi.pa165.facade.HouseFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import cz.muni.fi.pa165.dto.HouseCreateDTO;
-import cz.muni.fi.pa165.dto.HouseDTO;
-import cz.muni.fi.pa165.facade.HouseFacade;
+import java.util.Collection;
 
 /**
  * @author Jirka Kruml
@@ -25,39 +23,41 @@ public class HouseController {
     @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.GET)
     public Collection<HouseDTO> getAll() {
-        return Arrays.asList(
-                new HouseDTO().setId(1L).setAddress("New York").setName("Scary house").setHistory("history1"),
-                new HouseDTO().setId(2L).setAddress("Washington DC").setName("New house").setHistory("history2")
-                );
-//        return houseFacade.getAll();
+        return houseFacade.getAll();
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/name/{name}", method = RequestMethod.GET)
     public HouseDTO getByName(@PathVariable String name) {
         return houseFacade.getByName(name);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/address/{address}", method = RequestMethod.GET)
     public HouseDTO getByAddress(@PathVariable String address) {
         return houseFacade.getByAddress(address);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public HouseDTO getHouse(@PathVariable long id) {
         return houseFacade.getById(id);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.POST)
     public HouseDTO createHouse(@RequestBody HouseCreateDTO houseDTO) {
         return houseFacade.create(houseDTO);
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     public HttpStatus deleteHouse(@PathVariable long id) {
         houseFacade.delete(id);
         return HttpStatus.OK;
     }
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(method = RequestMethod.PUT)
     public HouseDTO updateHouse(@RequestBody HouseDTO houseDTO) {
         return houseFacade.update(houseDTO);
